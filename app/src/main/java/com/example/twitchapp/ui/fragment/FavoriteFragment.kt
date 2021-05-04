9bb5008fa983b2a127c8156e5463fe6e0456be4e
rikuyu <rikuyu5884@gmail.com>
2021-05-04 23:01:15 +0900

    fix layout files

--- app/src/main/java/com/example/twitchapp/ui/fragment/FavoriteFragment.kt
+++ app/src/main/java/com/example/twitchapp/ui/fragment/FavoriteFragment.kt
@@ -7,7 +7,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.twitchapp.R
