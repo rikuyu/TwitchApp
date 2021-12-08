38c955ece36175a37f08edff6850c0b02a143ee3
rikuyu <rikuyu5884@gmail.com>
2021-12-08 21:15:22 +0900

    SharedPreferences 関連クラス切り分け

---
+++ app/src/main/java/com/example/twitchapp/util/SharedPreferencesKey.kt
@@ -0,0 +1,13 @@
+package com.example.twitchapp.util
+
+object SharedPreferencesKey {
+
+    // SharedPreferenceのデータ保存ファイル名
+    const val SHARED_PPREFERENCE_FILE_NAME = "profile_data"
+
+    // プロフィールの名前
+    const val STORED_PROFILE_NAME = "STORED_PROFILE_NAME"
+
+    // プロフィールの画像
+    const val STORED_PROFILE_IMAGE_URI = "STORED_PROFILE_IMAGE_URI"
+}
